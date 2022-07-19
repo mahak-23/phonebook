de4a5aff4011a91faa2fb2469bde9c0cb25ba402
mahak-23 <107416996+mahak-23@users.noreply.github.com>
2022-07-19 11:51:38 +0530

    Add files via upload

---
+++ phonebook.mini.c
@@ -0,0 +1,299 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<stdlib.h>
+struct person
+{
+char name[35];
+
+long int pincode;
+ char area [35];
+
+char city[25];
+
+char state[35];
+
+long long int mble_no;
+
+char mail[100];
+};
+void menu();
+void start();
+void back();
+void addrecord();
+void listrecord();
+void modifyrecord();
+void deleterecord();
+void searchrecord();
+int main()
+{
+    
+    start();
+    return 0;
+}
+void back()
+{
+    start();
+}
+void start()
+{
+    menu();
+}
+void menu()
+{
+    system("cls");
+printf("\t\t**********WELCOME TO PHONEBOOK*************");
+
+printf("\n\n\t\t\t MENU\t\t\n\n");
+printf("\t1.Add New \t2.List \t3.Exit \n\t4.Modify \t5.Search\t6.Delete");
+
+switch(getch())
+{
+    case '1':
+
+                addrecord();
+    break;
+   case '2': listrecord();
+    break;
+    case '3': exit(0);
+    break;
+    case '4': modifyrecord();
+    break;
+    case '5': searchrecord();
+    break;
+    case '6': deleterecord();
+    break;
+    default:
+                system("cls");
+                printf("\nEnter 1 to 6 only");
+                printf("\n Enter any key");
+                getch();
+
+menu();
+}
+}
+        void addrecord()
+{
+        system("cls");
+        FILE *f;
+        struct person p;
+        f=fopen("project","ab+");
+        printf("\n Enter name: ");
+        scanf("%s",&p.name);
+        printf("\nEnter the address: ");
+        
+        printf("\nPincode: ");
+        scanf("%ld",&p.pincode);
+        printf("\nArea: ");
+        scanf("%s",&p.area);
+        printf("\nCity: ");
+        scanf("%s",&p.city);
+        printf("\nState: ");
+        scanf("%s",&p.state);
+        printf("\n Mobile no.:");
+        scanf("%lli",&p.mble_no);
+        printf("\nEnter e-mail:");
+         scanf("%s",&p.mail);
+        fwrite(&p,sizeof(p),1,f);
+
+      fflush(stdin);
+        printf("\nrecord saved");
+
+fclose(f);
+
+    printf("\n\nEnter any key");
+
+  getch();
+    system("cls");
+    menu();
+}
+void listrecord()
+{
+    struct person p;
+    FILE *f;
+f=fopen("project","rb");
+if(f==NULL)
+{
+printf("\nfile opening error in listing :");
+
+exit(1);
+}
+while(fread(&p,sizeof(p),1,f)==1)
+{
+     printf("\n\n\n YOUR RECORD IS\n\n ");
+        printf("\nName=%s \nAdress:\n\t Pincode=%ld \n\t Area=%s \n\t City=%s \n\t State=%s \nMobile no=%lli \nE-mail=%s",p.name,p.pincode,p.area,p.city,p.state,p.mble_no,p.mail);
+
+  getch();
+  system("cls");
+}
+fclose(f);
+ printf("\n Enter any key");
+ getch();
+    system("cls");
+menu();
+}
+void searchrecord()
+{
+    struct person p;
+FILE *f;
+char name[100];
+f=fopen("project","rb");
+if(f==NULL)
+{
+    printf("\n error in opening\a\a\a\a");
+    exit(1);
+
+}
+printf("\nEnter name of person to search\n");
+scanf("%s",&name);
+while(fread(&p,sizeof(p),1,f)==1)
+{
+    if(strcmpi(p.name,name)==0)
+    {
+        printf("\n\tDetail Information About %s",name);
+         printf("\nName=%s \nAdress:\n\t Pincode=%ld \n\t Area=%s \n\t City=%s \n\t State=%s \nMobile no=%lli \nE-mail=%s",p.name,p.pincode,p.area,p.city,p.state,p.mble_no,p.mail);
+
+     }
+    else
+        printf("file not found");
+
+}
+ fclose(f);
+  printf("\n Enter any key");
+
+  getch();
+    system("cls");
+menu();
+}
+void deleterecord()
+{
+    struct person p;
+    FILE *f,*ft;
+ int flag;
+ char name[100];
+ f=fopen("project","rb");
+ if(f==NULL)
+  {
+
+   printf("CONTACT'S DATA NOT ADDED YET.");
+
+  }
+ else
+ {
+  ft=fopen("temp","wb+");
+  if(ft==NULL)
+
+            printf("file opaning error");
+  else
+
+        {
+
+
+  printf("Enter CONTACT'S NAME:");
+ scanf("%s",&name);
+
+  fflush(stdin);
+  while(fread(&p,sizeof(p),1,f)==1)
+  {
+   if(strcmpi(p.name,name)!=0)
+    fwrite(&p,sizeof(p),1,ft);
+   if(strcmpi(p.name,name)==0)
+                flag=1;
+  }
+ fclose(f);
+ fclose(ft);
+ if(flag!=1)
+ {
+  printf("NO CONACT'S RECORD TO DELETE.");
+  remove("temp.txt");
+ }
+  else
+  {
+   remove("project");
+   rename("temp.txt","project");
+   printf("RECORD DELETED SUCCESSFULLY.");
+
+  }
+ }
+}
+ printf("\n Enter any key");
+
+  getch();
+    system("cls");
+menu();
+}
+
+void modifyrecord()
+{
+    int c;
+    FILE *f;
+    int flag=0;
+    struct person p,s;
+ char name[50];
+ f=fopen("project","rb+");
+ if(f==NULL)
+  {
+
+   printf("CONTACT'S DATA NOT ADDED YET.");
+   exit(1);
+
+
+  }
+ else
+ {
+     system("cls");
+  printf("\nEnter CONTACT'S NAME TO MODIFY:\n");
+            gets(name);
+            while(fread(&p,sizeof(p),1,f)==1)
+            {
+                if(strcmpi(name,p.name)==0)
+                {
+
+                    printf("\n Enter name: ");
+                    scanf("%s",&s.name);
+                    printf("\nEnter the address: ");
+        
+                     printf("\nPincode: ");
+                     scanf("%ld",s.pincode);
+                      printf("\t Area: ");
+                      scanf("%s",&s.area);
+                     printf("\nCity: ");
+                    scanf("%s",&s.city);
+                     printf("\nState: ");
+                    scanf("%s",&s.state);
+                   printf("\n Mobile no.:");
+                   scanf("%lli",&s.mble_no);
+                   printf("\nEnter e-mail:");
+                    scanf("%s",&s.mail);
+                   ;
+                    fseek(f,-sizeof(p),SEEK_CUR);
+                    fwrite(&s,sizeof(p),1,f);
+                    flag=1;
+                    break;
+
+
+
+                }
+                fflush(stdin);
+
+
+            }
+            if(flag==1)
+            {
+                printf("\n your data id modified");
+
+            }
+            else
+            {
+                    printf(" \n data is not found");
+
+            }
+            fclose(f);
+ }
+  printf("\n Enter any key");
+  getch();
+    system("cls");
+ menu();
+
+}
+
